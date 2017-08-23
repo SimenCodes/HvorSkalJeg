6b4c3a59688e2586677763be3e47c011663423c7
Simen Burud <sb@simen.codes>
2017-08-23 16:35:32 +0200

    Koble opp layouten mot Java-koden + legge til bibliotekene vi skal bruke

--- app/src/main/java/no/hackerspace_ntnu/hvorskaljeg/MainActivity.java
+++ app/src/main/java/no/hackerspace_ntnu/hvorskaljeg/MainActivity.java
@@ -2,12 +2,27 @@ package no.hackerspace_ntnu.hvorskaljeg;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  // Fields for our views, so we can reference them later on.
+  TextView courseView;
+  TextView locationView;
+  TextView timeView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Now that we have a layout, we find our views
+    courseView = (TextView) findViewById(R.id.courseView);
+    locationView = (TextView) findViewById(R.id.locationView);
+    timeView = (TextView) findViewById(R.id.timeView);
+
+    // … so that we can do stupid stuff with them!
+    locationView.setText("R2");
+    timeView.setText("D2");
   }
 }
